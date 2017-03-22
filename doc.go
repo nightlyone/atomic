f832639cca849984b02685135b2c3f546e94659d
Ingo Oeser <nightlyone@googlemail.com>
2017-03-22 10:08:33 +0100

    document limitations by pointing to the std lib

--- doc.go
+++ doc.go
@@ -1,2 +1,5 @@
 // Package atomic implements atomic operations as methods on types so they can be used more easily.
 package atomic
+
+// BUG(nightlyone): The implementation just wraps sync/atomic from the Go standard library,
+// so it suffers the same implementation limitations.
